f4364d8c6a857014608a94bc3b5412e592eefd7d
David Newkerk <keysplayer@gmail.com>
2015-07-28 18:13:23 -0700

    Update to use jQuery's more recent on syntax instead of delegate, adjust code for readability, add necessary line for paperclip to the comment model.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,5 @@ class Comment < ActiveRecord::Base
   has_attached_file :attachment
   has_attached_file :other_attachment
   validates :subject, :body, :presence => true
+  do_not_validate_attachment_file_type :attachment, :other_attachment
 end
